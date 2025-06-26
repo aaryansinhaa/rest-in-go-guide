926552d8409e3cc60e39f33bef2385031ca48bb8
Aaryan Kumar Sinha <138802020+aaryansinhaa@users.noreply.github.com>
2025-06-26 17:39:29 +0530

    Implemented the basic server

--- cmd/miyuki/main.go
+++ cmd/miyuki/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/aaryansinhaa/miyuki/pkg/config"
 )
@@ -13,11 +14,23 @@ func main() {
 	//Setup the database connection
 
 	//setup routes
+	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to Miyuki! Running in %s environment", cfg.Env)
+	})
 
+	server := http.Server{
+		Addr:    cfg.HTTPServerConfig.Address,
+		Handler: router,
+	}
 
 	//Start the HTTP server
-	fmt.Println("Miyuki server is running...")
-
+	fmt.Printf("Miyuki is running, open http://%s on your browser\n", cfg.HTTPServerConfig.Address)
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Failed to start server: %v\n", err)
+		return
+	}
 	// Additional setup and logic can be added here
 
 }
